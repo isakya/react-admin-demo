76425176fcf6a2fc1e9afd7a72a2abac211728a4
konglong <1016361268@qq.com>
2022-07-28 07:04:53 +0800

    搭建登陆表单from结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import { Card } from 'antd'
+import { Card, Form, Input, Checkbox, Button } from 'antd'
 import logo from '@/assets/logo.png'
 // 导入样式
 import './index.scss'
@@ -8,8 +8,32 @@ function Login() {
       <Card className='login-container'>
         <img className="login-logo" src={logo} alt="" />
         {/* 登陆表单 */}
+        <Form
+          initialValues={{ remember: true }}
+        >
+          <Form.Item
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className='login-checkbox-label'>同意隐私条款</Checkbox>
+          </Form.Item>
+
+          <Form.Item >
+            <Button block size='large' type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
-    </div>
+    </div >
   )
 }
 
