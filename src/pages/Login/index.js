9949f618f10248bf67b8d3f2c12922f0f5d2a8a5
konglong <1016361268@qq.com>
2022-07-28 06:55:57 +0800

    搭建登陆页面静态结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,14 @@
+import { Card } from 'antd'
+import logo from '@/assets/logo.png'
+// 导入样式
+import './index.scss'
 function Login() {
   return (
-    <div>
-      Login
+    <div className='login'>
+      <Card className='login-container'>
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登陆表单 */}
+      </Card>
     </div>
   )
 }
